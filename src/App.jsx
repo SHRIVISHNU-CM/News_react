e8dac182d85c3dcd8e1b7b7d31c4b47f8f5531c1
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-08 17:00:22 +0530

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Home from './Components/Home'
 
 function App() {
   return (
-    <div>
-      <p className='text-2xl text-red-600'>HI</p>
-    </div>
+    <>
+      <Home/>
+    </>
   )
 }
 
