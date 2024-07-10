571df14115cf7ae40bd76cd932006a38df763c51
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-10 23:06:52 +0530

    home page

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,55 @@
+import { useContext, useEffect, useState } from "react"
+import axios from "axios"
+import image from "../Image/no image.jpg"
+import { Link } from "react-router-dom"
+import { SearchContext } from "../ContextAPI/SearchContext"
+import ErrPage from "./ErrPage"
+function Cards() {
+    const [data, setData] = useState([])
+    const { val } = useContext(SearchContext)
+    const [err, SetErr] = useState("")
+    useEffect(() => {
+        axios.get(`https://newsapi.org/v2/everything?q=${val}&pageSize=30&apiKey=${import.meta.env.VITE_API_KEY}`)
+            .then((item) => {
+                setData(item.data.articles)
+                console.log(item.data)
+                console.log(item.data.articles[2])
+            })
+            .catch((e) => {
+                SetErr(e)
+            })
+    }, [])
+    return (
+        <>
+            <div className="flex flex-wrap gap-6 justify-center md:flex md:justify-around">
+                {
+                    err ? <ErrPage /> : data.map((itme, key) => {
+                        return (
+
+                            <div key={key} className="card bg-base-200 w-[400px] md:w-96 shadow-xl border-spacing-3">
+                                <div className="card ">
+                                    {itme.urlToImage ?
+                                        <img src={itme.urlToImage} className="w-[400px] h-[300px] bg-slate-500 " /> :
+                                        <img src={image} />
+                                    }
+                                    <h1 className="card-title font-body">{itme.title}</h1>
+                                    <div className="flex items-center gap-x-6">
+                                        {itme.author ?
+                                            <div className="badge badge-outline  m-2 h-min">
+                                                {itme.author}
+                                            </div> : null
+                                        }
+                                    </div>
+                                    <div className="absolute m-1 right-0">
+                                        <Link to={`/news/${key}`} className="btn ">Read More</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
+export default Cards
