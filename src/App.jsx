719b3624a98d60db89c1a7c44d13b3bfb45e770d
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-10 22:18:55 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Home from './Components/Home'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { SearchContext } from './ContextAPI/SearchContext'
+import FirstPage from './Components/FirstPage'
+import ReadMore from './Components/ReadMore'
 function App() {
+  const [technology,setTechnology]  = useState("")
+  const [sports,setSports]  = useState("")
+  const [trading,settrading]  = useState("")
+  const [bitcoin,setBitcoin]  = useState("")
+  const [val ,SetVal] = useState("")
+
   return (
     <>
-      <Home/>
+      <SearchContext.Provider value={{technology,sports,trading,bitcoin,val,SetVal}}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<FirstPage />} />
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/news/:id' element={<ReadMore/>}/>
+          </Routes>
+        </BrowserRouter>
+      </SearchContext.Provider>
+
+
     </>
   )
 }
