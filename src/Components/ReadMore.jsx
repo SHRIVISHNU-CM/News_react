e81c4f58e134418e5ef41860e4079ea4d0c1640b
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-10 23:07:10 +0530

    more page

---
+++ src/Components/ReadMore.jsx
@@ -0,0 +1,44 @@
+import { useContext, useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { SearchContext } from "../ContextAPI/SearchContext"
+import axios from "axios"
+import Navbar from "./Navbar"
+
+function ReadMore() {
+    const { val } = useContext(SearchContext)
+    const { id } = useParams()
+    const [data, setData] = useState("")
+    const [source,setSource] = useState("")
+    const api = `https://newsapi.org/v2/everything?q=${val}&pageSize=30&apiKey=${process.env.API_KEY}`
+    useEffect(() => {
+        axios.get(api)
+            .then((item) => {
+                setData(item.data.articles[id])
+                setSource(item.data.articles[id].source.name)
+                console.log(item.data.articles[id])
+                console.log(item.data.articles[id].source.name)
+
+
+            })
+
+            .catch(e => {
+                console.error(e)
+            })
+    }, [api, id])
+
+    return (
+        <>  
+            <Navbar/>
+            <div className="flex justify-center">
+                <img src={data.urlToImage} className="w-[500px] h-[500px]"/>
+            </div>
+            <p className=" font-cube">{data.description}</p>
+            <p className=" font-oswald">{data.content}</p>
+            <a href={data.url} target="_blank" className="btn btn-primary">visit Page</a>
+
+
+        </>
+    )
+}
+
+export default ReadMore
