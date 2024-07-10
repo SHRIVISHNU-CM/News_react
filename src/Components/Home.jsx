35d6ec09ff3f4e5436853f8c4ea313d949282139
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-10 22:21:25 +0530

    Home page component

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import Cards from "./Cards"
+import Navbar from "./Navbar"
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+            <Cards />
+        </>
+    )
+}
+
+export default Home
