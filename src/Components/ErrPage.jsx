3e8bfe2bc511841cfe30d590bf2da6e0f8418854
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-07-10 22:38:29 +0530

    height

--- src/Components/ErrPage.jsx
+++ src/Components/ErrPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function ErrPage(){
     return (
         <>  
-            <div>
+            <div className="flex items-center h-[50vh]">
                 <Link to={`/`} className="btn btn-secondary font-semibold">Go Back</Link>
             </div>
 
